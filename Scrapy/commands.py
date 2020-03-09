cb64a7583893446023a100e2492ac67dfdd5a74d
Gautam3994 <gautam3994@gmail.com>
2020-03-09 17:04:26 +0530

    commit

---
+++ Scrapy/commands.py
@@ -0,0 +1,9 @@
+"""
+scrapy - to see avaiable commands
+
+1) Benchmarking - scrapy bench
+2) Fetch HTML - scarpy fetch --nolog https://app.pluralsight.com/course-player?clipId=77710bd1-57a4-4ef9-9db3-2b69d4d05391
+3)Project specific settings - scrapy settings
+4)To see how scrapy view html content - scrapy view
+NORMALLY SCRAPY CAN DISPLAY ONLY STATIC CONTENT
+"""
